f2ef886e246e4b4b1d902f2f53601b5bda3d0891
AlbanoAyala <albanoayala@gmail.com>
2025-11-20 09:14:55 -0300

    Delete constants.ts

--- constants.ts
+++
@@ -1,8 +0,0 @@
-
-export const NOMBRES_CURVA_CBL = ['AMP3FT', 'CBLF', 'CBL'];
-export const UMBRAL_AMPLITUD_BUENA_MV = 10;
-export const UMBRAL_AMPLITUD_MALA_MV = 20;
-export const TOC_AMPLITUD_UMBRAL = 10;
-export const TOC_LONGITUD_MINIMA_M = 10;
-export const INTERVALO_LONGITUD_MINIMA_FT = 1.5;
-export const RANGO_SELLO_METROS = 3;
